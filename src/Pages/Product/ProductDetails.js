7e8fc3fa4a2e11c5254cc2d11710d21fed00ad40
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-09-11 17:42:02 +0600

    working on product-details

--- src/Pages/Product/ProductDetails.js
+++ src/Pages/Product/ProductDetails.js
@@ -19,19 +19,20 @@ const ProductDetails = () => {
     }, [id])
     console.log(product[0])
     return (
-        <div className='flex p-6 space-x-4 '>
-            <div>
+        <div className='flex p-6 space-x-4 row'>
+            <div className='col-5'>
                 <figure><img className='rounded-lg' src={product[0]?.strMealThumb} alt="menu" /></figure>
             </div>
-            <div>
+            <div className='col-7'>
                 <h1 className='text-3xl font-semibold'>{product[0]?.strMeal}</h1>
                 <p className='text-sm text-left'>{product[0]?.strInstructions}</p>
-                <StarRatings
+                <p className='inset-y-0 left-0 w-16 '> <StarRatings
+                    numberOfStars={1}
+                    starDimension="20px"
                     starRatedColor='rgb(249, 249, 7)'
-                    rating={2.403}
-                    starDimension="40px"
-                    starSpacing="15px"
-                />
+                    rating={1}
+                /> 4.3</p>
+                <p className='text-left'><b>Price :</b> $20</p>
             </div>
 
         </div>
