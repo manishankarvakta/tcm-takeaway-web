f769e8359503fa5215327fbce8763d55c66ec6eb
Atiqule Islam <evilrider555@gmail.com>
2022-09-15 02:04:19 +0600

    update menu category design..

--- src/Pages/Home/Category.js
+++ src/Pages/Home/Category.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import MenuSelectedFoods from './MenuSelectedFoods';
+import React from "react";
+import MenuSelectedFoods from "./MenuSelectedFoods";
 
 const Category = ({ category, setId }) => {
-    // console.log(category._id)
-    const { _id, name } = category;
+  // console.log(category._id)
+  const { _id, name } = category;
 
-    const loadCategoryWiseFoods = (id) => {
-
-        setId(id)
-
-    }
-    return (
-        <div>
-            <div onClick={() => loadCategoryWiseFoods(_id)} className='card bg-base-100 shadow-xl mx-2 pb-2 px-2 mb-2 hover:bg-orange-200 hover:scale-105'>
-                <figure className="px-2 pt-2">
-                    <img className='h-16 w-16 rounded-xl' src="https://i.ibb.co/6sy5871/front-view-burger-stand.jpg" alt="burger" />
-                </figure>
-                <p className='text-sm'>{name}</p>
-            </div>
-        </div>
-    );
+  const loadCategoryWiseFoods = (id) => {
+    setId(id);
+  };
+  // shadow-xl  hover:bg-orange-200 hover:scale-105
+  return (
+    <div>
+      <div
+        onClick={() => loadCategoryWiseFoods(_id)}
+        className="card bg-base-100 mx-2 pb-2 px-2 mb-2 cursor-pointer hover:text-red-600"
+      >
+        <figure className="px-2 pt-2">
+          <img
+            className="h-20 w-20 rounded-full"
+            src="https://i.ibb.co/6sy5871/front-view-burger-stand.jpg"
+            alt="burger"
+          />
+        </figure>
+        <p className="text-sm mt-3 font-bold">{name}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
--- src/Pages/Home/Menus.js
+++ src/Pages/Home/Menus.js
@@ -1,54 +1,66 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import Category from './Category';
-import { ArrowRightCircleIcon, ArrowLeftCircleIcon } from '@heroicons/react/24/solid';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Category from "./Category";
+import {
+  ArrowRightCircleIcon,
+  ArrowLeftCircleIcon,
+} from "@heroicons/react/24/solid";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from 'react-slick';
-
+import Slider from "react-slick";
 
 const Menus = ({ setId }) => {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        fetch('https://pos-api-v1.herokuapp.com/api/category/master/91')
-            .then(res => res.json())
-            .then(data => setCategories(data))
-    }, [])
+  useEffect(() => {
+    fetch("https://pos-api-v1.herokuapp.com/api/category/master/91")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
 
-    // const settings = {
-    //     dots: true,
-    //     infinite: false,
-    //     speed: 500,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
+  // const settings = {
+  //     dots: true,
+  //     infinite: false,
+  //     speed: 500,
+  //     slidesToShow: 3,
+  //     slidesToScroll: 1,
 
-    // };
+  // };
 
-    const settings = {
-        dots: true,
-        infinite: false,
-        autoplay: false,
-        autoplaySpeed: 3000,
-        slidesToShow: 8,
-        responsive: [
-            {
-                breakpoint: 900,
-                settings: {
-                    slidesToShow: 3,
-                },
-            }],
-    };
+  const settings = {
+    dots: false,
+    infinite: false,
+    lazyLoad: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    slidesToShow: 8,
+    slidesToScroll: 6,
+    prevArrow: (
+      <ArrowLeftCircleIcon className="h-5 w-5 fill-black cursor-pointer" />
+    ),
+    nextArrow: (
+      <ArrowRightCircleIcon className="h-5 w-5 fill-black cursor-pointer" />
+    ),
+    beforeChange: (current, next) => {
+      console.log(next);
+    },
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  };
 
-    const loadMenus = () => {
+  const loadMenus = () => {};
+  return (
+    <div className="mb-5 justify-center items-center lg:px-12">
+      <h1 className="font-semibold text-3xl">We Serve</h1>
 
-    }
-    return (
-        <div className='mb-5 justify-center items-center lg:px-12'>
-            <h1 className='font-semibold text-3xl'>We Serve</h1>
-
-            {/* <div className='flex justify-center items-center'>
+      {/* <div className='flex justify-center items-center'>
 
                 {
                     categories?.length > 0 ?
@@ -62,27 +74,23 @@ const Menus = ({ setId }) => {
                 }
             </div> */}
 
-            <div className='my-5 '>
-                <Slider {...settings}>
-                    {
-                        categories?.length > 0 ?
-                            categories?.map(category =>
-                                <Category
-                                    category={category}
-                                    setId={setId}
-                                ></Category>
-                            )
-                            :
-                            <></>
-                    }
-                </Slider>
-            </div>
-
-
-        </div>
-
-
-    );
+      <div className="my-5 mx-5">
+        {categories?.length > 0 ? (
+          <Slider {...settings}>
+            {categories?.map((category) => (
+              <Category
+                category={category}
+                key={category._id}
+                setId={setId}
+              ></Category>
+            ))}
+          </Slider>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Menus;
